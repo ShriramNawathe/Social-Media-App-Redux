b2a58fa52c860bbf93dd0b8502723de93d1158e3
Shriram26101999 <shriramnawathe@gmail.com>
2023-04-04 22:31:37 +0530

    ok

---
+++ src/api/posts.js
@@ -0,0 +1,8 @@
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+
+export const getPosts = async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+};
+// ok
